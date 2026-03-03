696236bf242c40f07a9964b1e3c408b4255a83bc
thedeep76 <amanisam835@gmail.com>
2026-03-03 17:32:55 +0100

    rule06

--- apps/analyzer/src/test.py
+++ apps/analyzer/src/test.py
@@ -10,3 +10,8 @@ def inc():
     global count
     count += 1
     return count
+
+# exemples de slicing (devraient déclencher PY006)
+big_list = list(range(1000))
+sub = big_list[100:200]
+step = big_list[::10]
