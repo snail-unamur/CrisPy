da3352e7760a000228af331213b5ea91702573b3
thedeep76 <amanisam835@gmail.com>
2026-03-11 17:46:16 +0100

    ajout rules

--- apps/analyzer/src/engine/analyzer.py
+++ apps/analyzer/src/engine/analyzer.py
@@ -3,6 +3,8 @@ from rules.PY001_numpy_array import NumpyArrayRule
 from rules.PY002_string_concat_in_loop import StringConcatInLoopRule
 from rules.PY003_exessive_global_variable import ExcessiveGlobalRule
 from rules.PY006_list_slicing import ListSlicingRule
+from rules.PY007_dict_items import DictItemsRule
+from rules.PY008_dict_get import DictGetRule
 
 
 def analyze_code(code: str):
@@ -13,6 +15,8 @@ def analyze_code(code: str):
         StringConcatInLoopRule(),
         ExcessiveGlobalRule(),
         ListSlicingRule(),
+        DictItemsRule(),
+        DictGetRule(),
     ]
 
     diagnostics = []
