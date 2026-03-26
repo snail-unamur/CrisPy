aa21192513b764b56cd73dbfebbe25412899e477
Blessing Tutka <benedicte.tutekamukuta@student.unamur.be>
2026-03-26 12:29:33 +0100

    fix(analyzer): fix rules import errors

--- apps/analyzer/src/engine/analyzer.py
+++ apps/analyzer/src/engine/analyzer.py
@@ -1,13 +1,4 @@
 import ast
-from rules.PY034_MemoryLocalityRule import MemoryLocalityRule
-from apps.analyzer.src.rules.PY030_ObjectOverheadRule import ObjectOverheadRule
-from apps.analyzer.src.rules.PY018_UnboundedGrowthRule import UnboundedGrowthRule
-from apps.analyzer.src.rules.PY015_DataFragmentationRule import DataFragmentationRule
-from rules.PY018_UnboundedGrowthRule import ListMembershipRule
-from rules.PY019_NestedLoopRule import NestedLoopRule
-from rules.PY020_GeneratorInsteadOfListRule import GeneratorInsteadOfListRule
-from rules.PY021_ListConcatInLoopRule import ListConcatInLoopRule
-from rules.PY022_PreferForRangeRule import PreferForRangeRule
 from rules.PY001_numpy_array import NumpyArrayRule
 from rules.PY002_string_concat_in_loop import StringConcatInLoopRule
 from rules.PY003_exessive_global_variable import ExcessiveGlobalRule
@@ -22,9 +13,14 @@ from rules.PY011_return_none_conditionally import ConditionalNoneReturnRule
 from rules.PY012_anonymous_tuple_return import AnonymousTupleReturnRule
 from rules.PY013_value_equality_with_is import IsForValueComparisonRule
 from rules.PY014_membership_on_list import MembershipOnListRule
-from rules.PY015_DataFragmentationRule import StringJoinRule
+from rules.PY015_DataFragmentationRule import DataFragmentationRule
 from rules.PY016_shadow_builtin import ShadowBuiltinRule
 from rules.PY017_any_all import AnyAllRule
+from rules.PY018_UnboundedGrowthRule import UnboundedGrowthRule
+from rules.PY019_NestedLoopRule import NestedLoopRule
+from rules.PY020_GeneratorInsteadOfListRule import GeneratorInsteadOfListRule
+from rules.PY021_ListConcatInLoopRule import ListConcatInLoopRule
+from rules.PY022_PreferForRangeRule import PreferForRangeRule
 from rules.PY023_WildcardImport import WildcardImportRule
 from rules.PY024_OpenWithoutWith import UseWithOpenRule
 from rules.PY025_TypeComparisonInsteadOfIsInstance import IsInstanceRule
@@ -32,10 +28,11 @@ from rules.PY026_DictInsteadOfDictComprehension import DictComprehensionRule
 from rules.PY027_NoTupleUnpacking import MultipleAssignmentUnpackingRule
 from rules.PY028_TypeInVariableName import TypeInVariableNameRule
 from rules.PY029_BadExceptOrder import ExceptOrderRule
-from rules.PY030_ObjectOverheadRule import MutableDefaultArgumentRule
+from rules.PY030_ObjectOverheadRule import ObjectOverheadRule
 from rules.PY031_LBYLInsteadOfEAFP import EAFPInsteadOfLBYLRule
 from rules.PY032_MapFilterInsteadOfListComprehension import ListComprehensionInsteadOfMapFilterRule
 from rules.PY033_set_membership import SetMembershipRule
+from rules.PY034_MemoryLocalityRule import MemoryLocalityRule
 
 
 
