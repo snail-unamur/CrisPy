d12eedf1b27fd0790b7cc97889d8d4cdb22cc92f
Blessing Tutka <benedicte.tutekamukuta@student.unamur.be>
2026-04-02 12:01:36 +0200

    feat(styles): add favicon

--- apps/docs/pages/_app.tsx
+++ apps/docs/pages/_app.tsx
@@ -3,10 +3,14 @@ import type { AppProps } from "next/app";
 import { AppLayout } from "@/components/Layout";
 import { AppHeader } from "@/components/header";
 import { ThemeProvider } from "@/providers/theme-provider";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <Head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+      </Head>
       <AppLayout header={<AppHeader />}>
         <article className="prose">
           <Component {...pageProps} />
